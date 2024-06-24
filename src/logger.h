46ac31fbf99273a60c490422a03bf4fcbc5a30f5
venitoxd666 <95440696+venitoxd666@users.noreply.github.com>
2024-06-24 18:10:15 +0200

    made formatting for logger, huge merge problems with this incoming
    Good luck with the merge

--- src/logger.h
+++ src/logger.h
@@ -1,16 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 
 #ifndef LOGGER_H
 #define LOGGER_H //LOGGER_G OOPS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+
 
 
 /// NEEDS CLEANUP AND FIXING/TESTING
 
 // #define str char* // It's just a prank bro
 
-enum TypeOfLog{
+enum TypeOfLog {
     NORMAL = 0, // Normal log, like to say what files it compiles
     WARNING = 1, // A warning
     ERROR = 2, // An error like, files that doesnt exist.
@@ -24,14 +27,17 @@ FILE *fileOpened;
 //header
 
 uint8_t init_logging();
-void log_to_file(char* message, logtype_t type);
+void log_to_file(char* message, logtype_t type, ...);
 void clear_log();
 void stop_logging();
+
+
 uint8_t is_logging() {
     if (fileOpened == NULL){
         return 0;
     }
-    return 1;}
+    return 1;
+}
 
 
 
@@ -43,15 +49,20 @@ uint8_t init_logging(){
     }
     fileOpened = fopen("./compilerLog.log", "w");
     if (fileOpened == NULL){
-        //log_to_file("The log file diesnt exists!", FATAL_ERR) ; //okay okay, this is a joke too
+        // log_to_file("The log file diesnt exists!", FATAL_ERR) ; //okay okay, this is a joke too
         return 1;
     }
     return 0;
 }
 
-void log_to_file(char* message, logtype_t type){
-    fprintf(fileOpened, "[%d]",type, message);
+void log_to_file(char* message, logtype_t type, ...){
+    va_list argptr;
+    va_start(argptr, format);
+    vfprintf(fileOpened, "[%d]",type, message, argptr);
+    va_end(argptr);
 }
+
+
 
 // I'm sorry
 void clear_file(){
