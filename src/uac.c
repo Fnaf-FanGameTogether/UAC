469a51f3b04242d686d4f9248cab04a58324ef38
Fnaf-FanGameTogether <157045788+Fnaf-FanGameTogether@users.noreply.github.com>
2024-06-25 10:57:54 +0200

    added builtin version printing as default behaviour when code is executed

--- src/uac.c
+++ src/uac.c
@@ -2,10 +2,11 @@
 #include "./macros_config.h"
 #include "compiler/compiler/tokenizer/tokenizer.h"
 #include "logger.h"
+#include "utils/version_info.h"
 
 int main(int argc, char** argv)
 {
-
+    fprint_builtin_platform_info(stdout);
     return 0;
 }
 
